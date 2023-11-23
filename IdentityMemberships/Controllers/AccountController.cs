ce875d47e9c73b101b90b872a19a82dc6b412285
MuammerKrc <muammer03karaca@gmail.com>
2023-11-23 03:43:10 +0300

    developed signIn controller and view

--- IdentityMemberships/Controllers/AccountController.cs
+++ IdentityMemberships/Controllers/AccountController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityStructureModel.IdentityModels;
+using IdentityStructureModel.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace IdentityMemberships.Controllers
 {
 	public class AccountController : Controller
 	{
-		public IActionResult SignIn()
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly UserManager<AppUser> _userManager;
+		public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
 		{
-			return View();
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
+
+		public IActionResult SignIn(string ReturnUrl)
+		{
+			TempData["ReturnUrl"] = ReturnUrl;
+			return View(new LoginViewModel());
+		}
+		[HttpPost]
+		public async Task<IActionResult> SignIn(LoginViewModel model)
+		{
+			string failedExceptionDesc = "Kullanıcı adı veya şifre hatalı";
+			bool lockoutUserWhenFailed = false;
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var user = await _userManager.FindByEmailAsync(model.Email.ToLower());
+			if (user == null)
+			{
+				ModelState.AddModelError("", failedExceptionDesc);
+				return View(model);
+			}
+
+			SignInResult signInResult = await _signInManager.PasswordSignInAsync(user, model.Password, true, false);
+
+			if (!signInResult.Succeeded)
+			{
+				ModelState.AddModelError("", failedExceptionDesc);
+				return View(model);
+			}
+
+			if (TempData["ReturnUrl"] != null && !string.IsNullOrEmpty(TempData["ReturnUrl"].ToString()))
+			{
+				return Redirect(TempData["ReturnUrl"].ToString());
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
