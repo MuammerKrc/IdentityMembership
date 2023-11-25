a2803300e76cf13a7cfb8df8e926c090b1ea108b
MuammerKrc <muammer03karaca@gmail.com>
2023-11-26 00:34:11 +0300

    added role list page

--- IdentityMemberships/Areas/Admin/Controllers/RoleController.cs
+++ IdentityMemberships/Areas/Admin/Controllers/RoleController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityStructureModel.IdentityModels;
+using IdentityStructureModel.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityMemberships.Areas.Admin.Controllers
 {
+	[Area("Admin")]
 	public class RoleController : Controller
 	{
-		public IActionResult Index()
+
+		private RoleManager<AppRole> _roleManager;
+
+		public RoleController(RoleManager<AppRole> roleManager)
 		{
-			return View();
+			_roleManager = roleManager;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var response = await _roleManager.Roles.ToListAsync();
+			return View(response);
 		}
 	}
 }
